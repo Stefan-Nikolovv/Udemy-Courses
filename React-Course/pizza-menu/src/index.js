e11a0f74814ca8a3adcdecf50e2363c14b752150
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-15 16:54:36 +0200

    adding data
    adding first component for now is static

--- React-Course/pizza-menu/src/index.js
+++ React-Course/pizza-menu/src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
+
+let data = pizzaData;
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
 }
 
+function Pizza(data) {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="img" />
+      <h2>Pizza Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
