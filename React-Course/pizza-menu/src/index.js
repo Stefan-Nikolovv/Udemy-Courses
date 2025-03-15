ed3688fb2868c4823b859db315cb1e32633f9aeb
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-15 17:44:04 +0200

    components
    adding components for first page and styling it

--- React-Course/pizza-menu/src/index.js
+++ React-Course/pizza-menu/src/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { pizzaData } from "./data";
-
+import "./index.css";
 let data = pizzaData;
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+function Header() {
+  //   const style = { color: "red", fontSize: "48px", textDecoration: "uppercase" };
+  //   return <h1 style={style}>Fast React Pizza Co.</h1>;
+
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open
+    </footer>
+  );
+  //   return React.createElement("footer", null, "We're currently open");
 }
 
 function Pizza(data) {
