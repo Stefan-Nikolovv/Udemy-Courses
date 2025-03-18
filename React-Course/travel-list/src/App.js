8696bc29ae5947b5b664961a87c4f55e0a0a719e
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-18 19:33:36 +0200

    travel list
    starting a new project

---
+++ React-Course/travel-list/src/App.js
@@ -0,0 +1,39 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> 🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-fomr">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already paced X</em>
+    </footer>
+  );
+}
