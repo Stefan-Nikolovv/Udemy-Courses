2ce6ba95b64a34fe20ab0a549686a3c6e36e6512
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-19 15:53:18 +0200

    adding state for stats
    adding state for showing diff message as condition rendering

--- React-Course/travel-list/src/App.js
+++ React-Course/travel-list/src/App.js
@@ -35,7 +35,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToogleItem={handleToogleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -115,10 +115,25 @@ function Item({ item, onDeleteItem, onToogleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already paced X</em>
+      <em>
+        {percentage === 100
+          ? "You got everyting! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already paced ${numPacked} (${percentage}%)
+        `}
+      </em>
     </footer>
   );
 }
