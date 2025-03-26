4cdbef2c8fdb30b6c1f0e04d368b2ea31d3ea7f3
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-26 14:56:05 +0200

    adding json server
    adding json server and fetching our data and using reducer

--- React-Course/react-quiz/src/App.js
+++ React-Course/react-quiz/src/App.js
@@ -1,9 +1,41 @@
+import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Something went wrong budy.");
+  }
+}
+
 export default function App() {
+  const [state, dispach] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispach({ type: "dataReceived", payload: data }))
+      .catch((err) => dispach({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
