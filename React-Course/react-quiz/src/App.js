52848c2e52227a4fc4f6a68157966c6d799d572a
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-26 16:58:14 +0200

    adding question component
    adding question component

--- React-Course/react-quiz/src/App.js
+++ React-Course/react-quiz/src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useReducer } from "react";
-import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -17,21 +17,23 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Something went wrong budy.");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispach] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => dispach({ type: "dataReceived", payload: data }))
-      .catch((err) => dispach({ type: "dataFailed" }));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
@@ -40,7 +42,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
