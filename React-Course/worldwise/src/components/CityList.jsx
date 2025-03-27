feb790e0d8acbe29812e22153ac24298888a7c7a
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-27 17:29:33 +0200

    adding message logic
    adding reusable component to the city list of there is not a city yet.

--- React-Course/worldwise/src/components/CityList.jsx
+++ React-Course/worldwise/src/components/CityList.jsx
@@ -1,9 +1,14 @@
 import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
-
+import Message from "./Message";
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by choosing a city on the map" />
+    );
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
