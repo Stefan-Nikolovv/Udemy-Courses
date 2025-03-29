30f81885f579b3e9789b32282484beb1b5e49a9d
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-03-29 14:37:06 +0200

    adding leaflet react
    adding map using react-leaflet

--- React-Course/worldwise/src/App.jsx
+++ React-Course/worldwise/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { CitiesProvider } from "./context/CitiesContext"; // Ensure this is correctly imported
+import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import HomePage from "./pages/HomePage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
-import CityList from "./components/CityList";
+import AppLayout from "./pages/AppLayout";
 import City from "./components/City";
+import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
-import { CitiesProvider } from "./context/CitiesContext";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
--- React-Course/worldwise/src/components/Map.jsx
+++ React-Course/worldwise/src/components/Map.jsx
@@ -1,10 +1,14 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [serachParms, setSearchParams] = useSearchParams();
-
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const lat = serachParms.get("lat");
   const lng = serachParms.get("lng");
 
@@ -15,9 +19,27 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+      <MapContainer
+        className={styles.map}
+        center={[lat, lng]}
+        zoom={5}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
