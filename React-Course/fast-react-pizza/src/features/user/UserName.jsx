f8417f0d40d828e011772c2c53dfa3381308f6d4
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-04 11:08:39 +0300

    create user handle

--- React-Course/fast-react-pizza/src/features/user/UserName.jsx
+++ React-Course/fast-react-pizza/src/features/user/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Stefan</div>;
+  const userName = useSelector((state) => state.user.username);
+
+  if (!userName) return;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default UserName;
