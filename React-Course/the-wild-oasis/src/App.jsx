94858048195e54d58cfe338db69bf8e8fb140759
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-05 17:18:25 +0300

    creating global style
    adding global styles component with css resetting.

--- React-Course/the-wild-oasis/src/App.jsx
+++ React-Course/the-wild-oasis/src/App.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello Word</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wold Oasis</H1>
+      </div>
+    </>
+  );
 }
 
 export default App;
