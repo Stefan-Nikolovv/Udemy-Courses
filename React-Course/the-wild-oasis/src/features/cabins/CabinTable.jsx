57551426f9b9e87c281d9ed7f2fda797708175aa
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-08 08:54:25 +0300

    custom hook for Cabins loading
    adding custom hook for handle loading of all cabins from BE.

--- React-Course/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ React-Course/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
-import { useQuery } from "@tanstack/react-query";
-import getCabins from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
 
@@ -29,14 +28,7 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const {
-    isLoading,
-    data: cabins,
-    error,
-  } = useQuery({
-    queryKey: ["cabin"],
-    queryFn: getCabins,
-  });
+  const { isLoading, cabins, error } = useCabins();
 
   if (isLoading) return <Spinner />;
   return (
