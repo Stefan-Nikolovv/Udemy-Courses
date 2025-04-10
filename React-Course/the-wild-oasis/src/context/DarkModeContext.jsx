6163079ec98963eb45f901246b14036b664b24ad
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-10 17:26:45 +0300

    loading required data for statistic
    loading Data for dashboard of statistics and logged it

--- React-Course/the-wild-oasis/src/context/DarkModeContext.jsx
+++ React-Course/the-wild-oasis/src/context/DarkModeContext.jsx
@@ -10,10 +10,10 @@ function DarkModeProvider({ children }) {
       if (isDarkMode) {
         document.documentElement.classList.add("dark-mode");
         document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.remove("dark-mode");
+        document.documentElement.classList.add("light-mode");
       }
-
-      document.documentElement.classList.add("light-mode");
-      document.documentElement.classList.remove("dark-mode");
     },
     [isDarkMode]
   );
--- React-Course/the-wild-oasis/src/pages/Dashboard.jsx
+++ React-Course/the-wild-oasis/src/pages/Dashboard.jsx
@@ -1,12 +1,23 @@
+import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
+import Spinner from "../ui/Spinner";
 import Row from "../ui/Row";
-
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
+import { useRecentStays } from "../features/dashboard/useRecentStays";
 function Dashboard() {
+  const { isLoading, bookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isStaysLoading } = useRecentStays();
+  if (isLoading || isStaysLoading) return <Spinner />;
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 }
 
