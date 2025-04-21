59fb119850c84abeb79b76bf699603310ef84e02
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-21 10:41:58 +0300

    handle reservation btn

--- Next-Project/the-wild-oasis-website/app/_componets/UpdateProfileForm.js
+++ Next-Project/the-wild-oasis-website/app/_componets/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { updateProfile } from "../_lib/actions";
+import { useFormState } from "react-dom";
 
 function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
@@ -56,11 +57,21 @@ function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormState();
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating.." : "Update profile"}
+    </button>
   );
 }
 
