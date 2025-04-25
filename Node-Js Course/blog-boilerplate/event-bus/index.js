1d805300ed5aa48e115604a8279994e2765f1145
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2025-04-25 14:29:21 +0300

    handle errors
    handle a errors for down api and when is going back we are put all missing data

--- Node-Js Course/blog-boilerplate/event-bus/index.js
+++ Node-Js Course/blog-boilerplate/event-bus/index.js
@@ -4,10 +4,10 @@ const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-
+const events = [];
 app.post("/events", (req, res) => {
   const event = req.body;
-
+  events.push(event);
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -25,7 +25,9 @@ app.post("/events", (req, res) => {
   });
   res.send({ status: "OK" });
 });
-
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 app.listen(4005, () => {
   console.log("Listening on 4005");
 });
